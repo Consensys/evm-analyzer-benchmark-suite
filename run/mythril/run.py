605f5305f91b0ee3c0c8c83079ae240820080638
rocky <rocky@users.noreply.github.com>
2018-07-30 15:46:23 -0400

    Lots of little improvements and simplifications...
    Simplify options:
     * --timeout option puts a cap on the time spend in running myth, as a result
     * YAML config "long" attribute (a boolean) becomes "run_time" which is a pessimistic value
       on the time to run a benchmark. If that is greater than --timeout (or its default)
       the benchmark is skipped.
      --debug and --verbose folded into --verbose with -v -v replacing --debug
     * Fix up YAML data for some tests
     * Add more verbose --help
     * comment Python functions better
     * tweak elapsed_str()

--- run/mythril/run.py
+++ run/mythril/run.py
@@ -10,24 +10,36 @@ import click
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
+# Maximim time, in seconds, that we allow Mythril to
+# take in analyzing a benchmark.
+DEFAULT_TIMEOUT=7.0
+
 
 def elapsed_str(elapsed):
-    ""
+    "Format `elapsed` into a human-readable string with hours, minutes and seconds"
     hours, rem = divmod(elapsed, 60 * 60)
     minutes, seconds = divmod(rem, 60)
+    secs_plural = 's' if seconds != 1.0 else ''
     hours = int(hours)
     minutes = int(minutes)
+    mins_plural = 's' if minutes != 1.0 else ''
     if hours:
-        return ("{} hours, {} minute(s), {:5.2f} second(s)"
-                .format(hours, minutes, seconds))
+        return ("{} hours, {} minute{}, {:5.2f} second{}"
+                .format(hours, minutes, mins_plural,
+                        seconds, secs_plural))
     elif minutes:
-        return ("{} minute(s), {:5.2f} second(s)"
-                .format(minutes, seconds))
+        return ("{} minute{}, {:5.2f} second{}"
+                .format(minutes, mins_plural,
+                        seconds, secs_plural))
     else:
-        return ("{:5.2f} second(s)"
-                .format(seconds))
+        return ("{:5.2f} second{}"
+                .format(seconds, secs_plural))
 
 def get_myth_prog():
+    """Return the mythril program name to run. Setting a name inenvironent variable MYTH
+    takes precidence of the vanilla name "myth". As a sanity check, try
+    running this command with --version to make sure it does something.
+    """
     myth_prog = os.environ.get('MYTH', 'myth')
     cmd = [myth_prog, '--version']
     s = subprocess.run(cmd, stdout=subprocess.PIPE)
@@ -38,12 +50,15 @@ def get_myth_prog():
     # FIXME: check version
     return myth_prog
 
-def run_myth(myth_prog, sol_file, debug):
+def run_myth(myth_prog, sol_file, debug, timeout=DEFAULT_TIMEOUT):
     cmd = [myth_prog, '-x', '-o', 'json', '{}:Benchmark'.format(sol_file)]
     if debug:
         print(' '.join(cmd))
     start = time.time()
-    result = subprocess.run(cmd, stdout=subprocess.PIPE)
+    try:
+        result = subprocess.run(cmd, stdout=subprocess.PIPE, timeout=timeout)
+    except subprocess.TimeoutExpired:
+        result = None
     elapsed = (time.time() - start)
     return elapsed, result
 
@@ -58,18 +73,23 @@ def get_benchmark_yaml(mydir, suite_name, debug):
     return testsuite_conf
 
 @click.command()
-@click.option('--long-run', '-l', default=False,
-              help="Run longer-running tests.")
-@click.option('--verbose', '-v', default=False,
-              help="Run longer-running tests.")
-@click.option('--debug', '-d', default=False,
-              help="Run longer-running tests.")
-def run_benchmark_suite(long_run, verbose, debug):
+@click.option('--verbose', '-v', count=True,
+              help="More verbose output; use twice for the most verbose output.")
+@click.option('--timeout', '-t', type=float,
+              default=DEFAULT_TIMEOUT,
+              help="Maximum time allowed on any single benchmark.")
+def run_benchmark_suite(verbose, timeout):
+    """Run Mythril on a benchmark suite.
+
+    If you set environment variable MYTH, that will be used a the myth CLI command to
+    invoke. If that is not set, we run using "myth".
+    """
     myth_prog = get_myth_prog()
     if not myth_prog:
         sys.exit(1)
 
     mydir = Path(__file__).parent.resolve()
+    debug = verbose == 2
     testsuite_conf = get_benchmark_yaml(mydir, 'suhabe', debug)
     testsuite_benchdir = mydir.parents[1] / 'benchmarks' / 'suhabe'
     os.chdir(testsuite_benchdir)
@@ -77,7 +97,7 @@ def run_benchmark_suite(long_run, verbose, debug):
     # Zero counters
     unconfigured = invalid_execution = error_execution = 0
     ignored_benchmarks = unfound_issues = benchmarks = 0
-    passed = 0
+    timed_out = passed = 0
     total_time = 0.0
 
     # for sol_file in ['eth_tx_order_dependence_minimal.sol']:
@@ -88,22 +108,28 @@ def run_benchmark_suite(long_run, verbose, debug):
         test_name = Path(sol_file).stem
         expected_data = testsuite_conf.get(test_name, None)
         if expected_data:
+            run_time = expected_data.get('run_time', timeout)
             if expected_data.get('ignore', None):
                 print('Benchmark "{}" marked for ignoring; reason: {}'
                       .format(test_name, expected_data['reason']))
                 ignored_benchmarks += 1
                 continue
-            elif expected_data.get('long', False) or long_run:
-                print('Benchmark "{}" skipped because it takes a long time to run'
-                      .format(test_name))
+            elif timeout < run_time:
+                print('Benchmark "{}" skipped because it is noted to take a long time; {} seconds'
+                      .format(test_name, run_time))
                 ignored_benchmarks += 1
                 continue
 
         cmd = [myth_prog, '-x', '-o', 'json', '{}:Benchmark'.format(sol_file)]
-        if debug:
+        if verbose:
             print(' '.join(cmd))
 
-        elapsed, s = run_myth(myth_prog, sol_file, debug)
+        elapsed, s = run_myth(myth_prog, sol_file, debug, timeout)
+        if s is None:
+            print('Benchmark "{}" timed out after {}'.format(test_name, elapsed_str(elapsed)))
+            timed_out += 1
+            continue
+
         total_time += elapsed
         print(elapsed_str(elapsed))
 
@@ -140,7 +166,6 @@ def run_benchmark_suite(long_run, verbose, debug):
                 print("Expecting to find {} issue(s), got {}"
                       .format(len(expected_issues), len(data_issues)))
                 error_execution += 1
-                benchmark_success = False
                 pp.pprint(data_issues)
                 print("=" * 30)
                 continue
@@ -182,10 +207,10 @@ def run_benchmark_suite(long_run, verbose, debug):
         pass
     print('-' * 40)
 
-    print("\nSummary: {} benchmarks; {} passed {} unconfigured, {} invalid execution, "
-          "{} errored, {} ignored."
+    print("\nSummary: {} benchmarks; {} passed {}, unconfigured, {} invalid execution, "
+          "{} errored, {} timed out, {} ignored."
           .format(benchmarks, passed, unconfigured, invalid_execution, error_execution,
-                  ignored_benchmarks))
+                  timed_out, ignored_benchmarks))
     print("Total elapsed execution time: {}".format(elapsed_str(total_time)))
 
 if __name__ == '__main__':
