b0297c570e715a5d5ba8baee1538a619326dd415
rocky <rocky@users.noreply.github.com>
2018-08-02 16:47:55 -0400

    Move forward on report generator

--- html/run_manticoregit_integer_overflow_bytes_param_16d6b8b6-934c-4ba8-b99a-2211d5763820/integer_overflow_bytes_param.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.8;
-
-contract Benchmark {
-    function get(bytes key) public pure returns (bytes32) {
-        return keccak256(key);
-    }
-}
--- html/run_oyentepip_assert_constructor_0f071cbd-7dda-48e0-bc66-808f0d7a0256/assert_constructor.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.19;
-
-contract Benchmark {
-    function Benchmark() public {
-        assert(false);
-    }
-}
